f286a6cbc80f5d9bd5a45a5eaa9344df9a9d11cc
wen2li3 <wen2li3@users.noreply.github.com>
2015-05-24 12:01:47 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -6,15 +6,17 @@
 #The makeCacheMatrix function creates a special "matrix",
 #which is a list containing a function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix())
+{
 i <- NULL
+j <- NULL
 
-#1 set the value of the matrix
+#1 set the value of i and j to NULL (default if cacheSolve not used)
 
 set <- function(y)
 {
-x <<- y
-i <<- NULL
+x <<- j
+j <<- NULL
 }
 
 #2 get the value of the matrix
@@ -28,14 +30,28 @@ setinverse <- function(inverse) i <<- inverse
 #4 get the value of the inverse of the matrix
 
 getinverse <- function() i
-list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+list(
+set = set,
+get = get,
+setinverse = setinverse,
+getinverse = getinverse
+)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        #The cacheSolve function calculates the inverse of the special "matrix"
+#"matrix" created with makeCacheMatrix
+
+cacheSolve <- function(x, ...)
+{
+
+if (nrow(x$get()) != ncol(x$get())) {
+        message("No possible to invert a rectangular matrix")
+        return()
+    }
+
+#The cacheSolve function calculates the inverse of the special "matrix"
 #Return a matrix that is the inverse of 'x'
 i <- x$getinverse()
 
@@ -43,9 +59,11 @@ i <- x$getinverse()
 
 if(!is.null(i))
 {
+if(x$setmatrix() == x$getmatrix()){
+
 message("getting cached data")
 return(i)
-
+}
 #it gets the inverse from the cache and skips the computation
 
 }
@@ -56,6 +74,4 @@ data <- x$get()
 i <- solve(data, ...)
 x$setinverse(i)
 i
-        
-        ## Return a matrix that is the inverse of 'x'
 }
